97d08ef6122b1fda907b9c3aa75a5546039950ba
jose8991 <joseluisgm1998@gmail.com>
2022-09-22 22:44:26 -0500

    estudiando docker compose

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <button @click="prueba">revisar</button>
+</template>
+
+<script setup>
+
+const hola = 'Hello from script setup!'
+function prueba(){
+  console.log(hola)
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
